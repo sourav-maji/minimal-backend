2857e6d612a76833cc7e113e66eb89482c84ed2a
sourav-maji <s722143@gmail.com>
2025-06-19 10:01:58 +0530

    Register Route setup

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { login, protectedRoute } from '../controllers/auth.controller.js';
+import { register, login, protectedRoute } from '../controllers/auth.controller.js';
 import { authenticate } from '../utils/auth.middleware.js';
 
 const router = express.Router();
 
+router.post('/register', register);
 router.post('/login', login);
 router.get('/protected', authenticate, protectedRoute);
 
 export default router;
-  
